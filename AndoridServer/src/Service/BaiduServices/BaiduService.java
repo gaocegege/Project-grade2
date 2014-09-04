aa5cd9d7253c5f994658e420d9a455aecac1e7a7
JerryLX <724733136@qq.com>
2014-09-04 20:28:46 +0800

    百度摘要

--- AndoridServer/src/Service/BaiduServices/BaiduService.java
+++ AndoridServer/src/Service/BaiduServices/BaiduService.java
@@ -30,6 +30,20 @@ public class BaiduService {
 	public void setContentDAO(ContentDAO contentDAO) {
 		this.contentDAO = contentDAO;
 	}
+	
+	public String searchForSum(String title) throws IOException{
+		String urlA = "http://news.baidu.com/ns?word=";
+		String urlB = "&tn=news&from=news&cl=2&rn=20&ct=1";
+		String url = urlA + title + urlB;
+		Document doc;
+		doc = Jsoup.connect(url).get();
+		Element news = doc.getElementById("1");
+		Element sum = news.getElementsByClass("c-summary").get(0);
+		String preresult = sum.text();
+		int index = preresult.lastIndexOf(".");
+		String result = preresult.substring(0,index+1);
+		return result;
+	}
 
 
 	public List<Content> search(String searchContext, int id)
@@ -185,51 +199,17 @@ public class BaiduService {
 		return result;
 	}
 	public static void main(String args[]) throws IOException {
-		String searchContext = "广东 可疑男子";
-		int id = 0;
-		List<Content> result = new ArrayList<Content>();
+		String title = "广东 可疑男子";
 		String urlA = "http://news.baidu.com/ns?word=";
-		String urlB = "&tn=news&from=news&cl=2&rn=20&ct=0&clk=sortbytime";
-		
-		String url = urlA + searchContext + urlB;
+		String urlB = "&tn=news&from=news&cl=2&rn=20&ct=1";
+		String url = urlA + title + urlB;
 		Document doc;
 		doc = Jsoup.connect(url).get();
-		Element news = doc.getElementById("content_left");
-		Elements newsList = news.getElementsByTag("li");
-		int num;
-		if (newsList.size() > 20)
-			num = 20;
-		else
-			num = newsList.size();
-		for (int i = 0; i < num; i++) {
-			Content buf = new Content();
-			NewsContent newsContent = new NewsContent();
-			String id_str = newsList.get(i).attr("id").toString();
-			System.out.println(id_str);
-			String title = newsList.get(i).getElementsByTag("h3").get(0).text();
-			String newsUrl = newsList.get(i).getElementsByTag("h3").get(0)
-					.getElementsByTag("a").get(0).attr("href");
-
-			String author_time = newsList.get(i).getElementsByTag("span")
-					.text();
-			String imageUrl = null;
-			Elements pic = newsList.get(i).getElementsByTag("img");
-			if(pic.size()!=0)
-				imageUrl = pic.get(0).attr("src");
-			String f = author_time.substring(0, 1);
-			String[] list = author_time.split(f);
-			String from = list[1];
-			if (from.equals(""))
-				from = null;
-			String time = list[2];
-			System.out.println(title);
-			buf.setFrom(from);
-			buf.setId(Integer.parseInt(id_str));
-			buf.setTime(time);
-			buf.setTitle(title);
-			buf.setUrl(newsUrl);
-			buf.setImageUrl(imageUrl);
-			result.add(buf);
-		}
+		Element news = doc.getElementById("1");
+		Element sum = news.getElementsByClass("c-summary").get(0);
+		String preresult = sum.text();
+		int index = preresult.lastIndexOf(".");
+		String result = preresult.substring(0,index+1);
+		System.out.println(result);
 	}
 }
